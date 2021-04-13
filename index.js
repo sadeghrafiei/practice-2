27d238e76dba286c5776ced472a36ff20ab0a8fe
Sadegh Rafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-04-13 16:43:21 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {AppRegistry} from 'react-native';
-import {enableScreens} from 'react-native-screens';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 
 import {store, persistor} from 'store/index';
@@ -12,14 +11,10 @@ import App from './app/App';
 
 import {name as appName} from './app.json';
 
-enableScreens();
-
 const Elegant = () => (
-  <Provider store={store}>
-    <PersistGate loading={<Loading />} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <>
+    <App />
+  </>
 );
 
 AppRegistry.registerComponent(appName, () => Elegant);

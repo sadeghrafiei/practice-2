dbc8d9630694e3c13648a4e8002137f5c288a74d
Sadegh Rafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-04-27 01:58:52 +0430

    Update auth.js

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import Login from '../view/pages/login/index';
+import VerifyCode from '../view/pages/login/FormValid/auth/verify';
+import { HomeBottomTab } from './main';
+import Login from '../view/pages/login/FormValid/auth/login';
+import Register from '../view/pages/login/FormValid/auth/register';
 
 const Stack = createStackNavigator();
 
 export const AuthStack = () => {
   return (
     <Stack.Navigator headerMode={'none'}>
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Login" component={Register} />
+      <Stack.Screen name="SignIn" component={Login} />
+      <Stack.Screen name="Verify" component={VerifyCode} />
+      <Stack.Screen name="HomeBottomTab" component={HomeBottomTab} />
     </Stack.Navigator>
   );
 };

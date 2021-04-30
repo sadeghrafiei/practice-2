4e7b64149be529351b015c16c38437c2c705d3c9
Sadegh Rafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-05-01 01:30:26 +0430

    Update index.js

--- app/view/pages/home/instagram/components/contentContainer/Profile/index.js
+++ app/view/pages/home/instagram/components/contentContainer/Profile/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import {
   StyleSheet,
-  View,
+  View,Image,Text,TouchableOpacity 
 } from 'react-native';
-import ProfileDetails from './ProfileDetails';
+import Icon2 from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
+import images from '../../../assets/images';
 
 const Profile = ({item}) => {
+  const navigation = useNavigation(); 
   return (
     <View style={styles.postTopWrapper}>
-        <ProfileDetails item={item}/>
+        <View style={styles.postAuthorWrapper}>
+          <TouchableOpacity
+          onPress={() => {
+          navigation.navigate('UserAccountStack');
+        }}>
+          <Image
+            source={images.profile.profileSadegh}
+            resizeMode="cover"
+            style={styles.postProfileImg}
+            />
+            </TouchableOpacity>
+          <Text style={styles.postUsername}>{item.username}</Text>
+          <Icon2
+          style={styles.postMoreIcon}
+          name="ellipsis-v"
+          size={20}
+          color="black"
+        />
+        </View>
     </View>
   );
 };
@@ -19,5 +40,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  postAuthorWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  postProfileImg: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    margin: 8,
+  },
+  postUsername: {
+    fontWeight: '600',
+  },
+  postMoreIcon: {
+    transform: [{rotate: '90deg'}],
+    marginRight: 8,
+    marginStart: 220
   },
 });
